7a347ba063518705046efe8e991c5841df478a45
Robert Lee <bobbydwise1@hotmail.com>
2019-07-20 20:31:18 -0700

    Add node code.

--- scripts.js
+++ scripts.js
@@ -17,6 +17,8 @@ For example, the following tree has 5 unival subtrees:
  / \
 1   1
 
+//Note to self... there might be a typo above?  Instead of 5 unival subtrees, I think there are only 4?  Unless you count the entire tree itself as a subtree, then that is 5.
+
 */
 
 /* problem reworded:??
@@ -47,8 +49,19 @@ const node2 = new myNode(2, node1, node3);
 const node5 = new myNode(5);
 const node7 = new myNode(7, node2, node5);
 const node6 = new myNode(6);
-const node8 = new myNode(8, node6,node7);
+const binroot = new myNode(8, node6, node7);
 
+//suppose we do the same for the unival tree:
+
+const root_right_left_left = new myNode(1);
+const root_right_left_right = new myNode(1);
+const root_right_left = new myNode(1, root_right_left_left, root_right_left_right);
+const root_right_right = new myNode(0);
+const root_left = new myNode(1);
+const root_right = new myNode(0, root_right_left, root_right_right);
+const root = new myNode(0, root_left, root_right);
+
+//We note that what it is really asking for is how many nodes are where the left and right are both defined as 'none'.
 
 $(document).ready(function() {
   $('#output-section-1').text(1);
